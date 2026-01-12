8cb39ab7928d1a1d5c6983764ae4b51149c6c564
David Purton <dcpurton@marshwiggle.net>
2026-01-12 14:41:59 +1030

    Merge bibleref-sbl as a subtree

---
+++ bibleref-sel/build.lua
@@ -0,0 +1,7 @@
+-- Build configuration for bibleref-sbl
+
+module = "bibleref-sbl"
+checkformat = "latex"
+stdengine    = "pdftex"
+checkengines = {"pdftex"}
+typesetsuppfiles = {"example-ancient-sources-index.pdf"}
